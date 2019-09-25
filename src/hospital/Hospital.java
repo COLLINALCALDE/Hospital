8d23aead8d43e820c9efed1199d8f517810a76f5
COLLINVALERIA <51164762+COLLINVALERIA@users.noreply.github.com>
2019-09-25 13:01:04 -0500

    version1.0

---
+++ src/hospital/Hospital.java
@@ -0,0 +1,20 @@
+
+package hospital;
+
+import java.util.Date;
+
+
+public class Hospital {
+
+    private int numeroDeHabitaciones;
+    private Date mesConMasPacientes;
+    private String salaMasVisitada;
+    private boolean pacientesConENfermedadesTerminales;
+    private String pacientesEnUnDia;
+       
+            
+   public static void main(String[] args) {
+       
+ }
+    
+
